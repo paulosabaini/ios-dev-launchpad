39cd0909aefcf8df77f99eb395e20e40afe56947
Paulo Sabaini <pasabaini@gmail.com>
2023-01-30 11:41:21 -0300

    finish playing card app

--- CardWorkout/CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardWorkout/CardSelectionVC.swift
@@ -10,23 +10,34 @@ import UIKit
 class CardSelectionVC: UIViewController {
     
     @IBOutlet var cardImageView: UIImageView!
+    var cards: [UIImage] = Deck.cards
     
-    @IBOutlet var buttons: [UIButton]!
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        for button in buttons {
-            button.layer.cornerRadius = 8
-        }
+        startTimer()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
     }
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
     }
 
     @IBAction func restartButtonTapped(_ sender: UIButton) {
-    }
-    
-    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
     }
 }
