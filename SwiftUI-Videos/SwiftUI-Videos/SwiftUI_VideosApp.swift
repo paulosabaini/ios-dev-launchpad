ead99c2abd578b0efec8c37af9d9b8865fcb908a
Paulo Sabaini <pasabaini@gmail.com>
2023-01-31 14:06:50 -0300

    add swiftui overview app

---
+++ SwiftUI-Videos/SwiftUI-Videos/SwiftUI_VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideosApp.swift
+//  SwiftUI-Videos
+//
+//  Created by Paulo Antonio Sabaini on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
